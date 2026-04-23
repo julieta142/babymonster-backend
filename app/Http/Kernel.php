ddd2ca229a8e929b876340d1f8e828b38a8cda32
Julia <julietahakobyan10@gmail.com>
2026-04-23 22:53:03 +0400

    Comment out Sanctum stateful middleware to fix CSRF

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -25,11 +25,11 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
-        'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
+       'api' => [
+    // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,  // COMMENT THIS OUT
+    'throttle:api',
+    \Illuminate\Routing\Middleware\SubstituteBindings::class,
+],
     ];
 
     protected $routeMiddleware = [
